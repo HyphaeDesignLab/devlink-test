ffcc68915bda1d4bb226efd32dd4e05b8a1e7ea6
Ivan Velev <vanecat@gmail.com>
2023-11-14 13:35:39 -0800

    ignore webflow rc

--- src/.webflowrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: "https://api.webflow.com",
-    rootDir: "./devlink",
-    siteId: "64dc102f0fe9b9caf0623028",
-    authToken: "286e52bac43c630a1cefe6db8a044159bbbf8b0c4143d5f8b6b1234e96334e9f", // An environment variable is recommended for this field.
-    cssModules: true
-}
