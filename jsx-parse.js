6ae6bf3f28fa742806e95290c45f3b18786d2308
Ivan Velev <vanecat@gmail.com>
2023-11-16 18:28:05 -0800

    jsx parser: update AST and generate JSX code out of it

--- jsx-parse.js
+++ jsx-parse.js
@@ -1,14 +1,17 @@
-const babel = require('@babel/core');
+const generator = require('@babel/generator').default;
 const parser = require('@babel/parser');
+const babelTypes = require('@babel/types');
 const traverse = require('@babel/traverse').default;
+const shell = require('shelljs');
 const fs = require('fs');
 
 if (!process.argv[2]) {
     console.log("Please enter the JSX file path as the first .js script argument")
     process.exit(1);
 }
-var shell = require('shelljs');
-const execObj = shell.exec(`tr '\\n' '~' < ${process.argv[2]} | sed -E -e 's/.+(<body .+<\\/body>).+/\\1/' -e 's@<script [^>]+></script>@@g' | tr '~' '\\n'`);
+
+const cmd = `tr '\\n' '~' < ${process.argv[2]} | sed -E -e 's/.+(<body .+<\\/body>).+/\\1/' -e 's@<script [^>]+></script>@@g' | tr '~' '\\n'`;
+const execObj = shell.exec(cmd, { silent: true});
 
 // Parse JSX code into AST
 const ast = parser.parse(execObj.stdout, {
@@ -26,11 +29,12 @@ traverse(ast, {
         if (!value) {
             return;
         }
+        const element = {
+            type: 'text',
+            value
+        };
         if (path.node._parent) {
-            path.node._parent.children.push({
-                type: 'text',
-                value: value
-            });
+            path.node._parent.children.push(element);
         } else {
             tree.push(element);
         }
@@ -44,8 +48,14 @@ traverse(ast, {
                 name: attr.name.name,
                 value: attr.value ? attr.value.value : null,
             })),
-            children: [],
+            children: []
         };
+        if (node.name.name === 'Nav') {
+            node.attributes.push(babelTypes.jsxAttribute(
+                babelTypes.jsxIdentifier('a1'),
+                babelTypes.stringLiteral('v1')
+            ))
+        }
 
         if (path.node.children.length > 0) {
             path.node.children.forEach(child => {
@@ -62,4 +72,6 @@ traverse(ast, {
 });
 
 // Output the tree
-console.log(JSON.stringify(tree, null, 2));
+fs.writeFileSync('test.json', JSON.stringify(tree, null, 2));
+const generatedCode = generator(ast, {}, execObj.stdout);
+fs.writeFileSync('test.js', generatedCode.code);
